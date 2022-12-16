587c1ab2e66698c30d7d718ea8ab4734afe9ec54
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2022-12-16 13:26:43 +0400

    add task1

---
+++ HomeWork01/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//ДЗ Задача 1: найти меньшее из двух чисел.
+Console.Write("Вычисление меньшего из двух чисел");
+Thread.Sleep(1500);
+Console.Clear();
+Console.Write("Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Clear();
+Console.Write("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.Clear();
+int min = 0;
+
+if(x < y){
+    min = x;
+    Console.WriteLine($"Минимальное число: {min}");
+}
+else if(x > y){
+    min = y;
+    Console.WriteLine($"Минимальное число: {min}");
+}
