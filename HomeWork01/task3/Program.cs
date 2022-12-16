d49b794f2091e1d588adb4d07b5afccb3c5e9fca
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2022-12-16 13:29:47 +0400

    add task3

---
+++ HomeWork01/task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//ДЗ Задача 3:Провека числа на четность.
+Console.Write("Проверка числа на четность");
+Thread.Sleep(1500);
+Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Clear();
+
+if(x % 2 == 0){
+    Console.WriteLine($"число: {x}, четное ");  
+}
+else{
+    Console.Write($"число: {x}, не четное ");
+} 
