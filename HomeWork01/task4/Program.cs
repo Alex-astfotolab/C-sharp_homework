f5a3a4b861a5fddc9ae0a9c55d4b0a6ab5e132a9
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2022-12-16 14:27:23 +0400

    add task4

---
+++ HomeWork01/task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Поиск всех четных чисел в диапазоне от 1 до N ");
+Thread.Sleep(1500);
+Console.Clear();
+Console.WriteLine("Введи число: ");
+int usernum = int.Parse(Console.ReadLine()!);
+Console.Clear();
+Console.WriteLine("последовательность четных чисел: ");
+int num = 0;
+
+while(num <= usernum)
+{
+    if(num % 2 == 0){
+    Console.WriteLine(num);
+    num++;
+} 
+    else{
+    num++;
+    }
+}
