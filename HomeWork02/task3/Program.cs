c1e33cee8542481a9b06ca15e62675594476c780
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2022-12-20 15:40:47 +0400

    add task3

--- HomeWork02/task3/Program.cs
+++ HomeWork02/task3/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//ДЗ 3: определение выхлдной\будний дени с провверкой ввода менше 1го и больше 7ми
+Console.Clear();
+Console.Write("Введите день недели: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Clear();
+
+if ((x <= 7) && (x > 0))
+{
+    if ((x > 0) && (x <= 5))
+    {
+        Console.WriteLine("будний день");
+    }
+    if ((x > 5) && (x <= 7))
+        Console.WriteLine("выходной день");
+}
+else
+{
+    Console.WriteLine("не может быть меньше 1го или больше 7 дней в неделе!");
+}
