e6c6fde3fe19ba4e3364c1d140d83111b9477690
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2022-12-20 14:31:53 +0400

    add task1

---
+++ HomeWork02/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//ДЗ задача1: нахождение 2ой цифры трехзначного числа с проверкой ввода на >< 3х знаков
+Console.Clear();
+Console.Write("Введите трехзначтное число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Clear();
+
+if((x > 99) && (x < 1000))
+{
+string num = x.ToString();
+
+Console.WriteLine($"второй знак числа {x}, число {num[1]}");
+}
+else{
+    Console.WriteLine($"число {x} не трехзначное!!!");
+}
