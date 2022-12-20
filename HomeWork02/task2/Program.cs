26bfc42df67ff00d8a1c56633abfb52a317289d0
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2022-12-20 15:26:09 +0400

    add task2

---
+++ HomeWork02/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿//ДЗ 2: нахождение третьего символа числа с проверкой на отсутствие третьего символа
+Console.Clear();
+int x = new Random().Next(1, 100000);
+
+if(x > 99)
+{
+string num = x.ToString();
+
+Console.WriteLine($"{x} -> {num[2]}");
+}
+else{
+    Console.WriteLine($"{x} -> нет третьего знака!!!");
+}
