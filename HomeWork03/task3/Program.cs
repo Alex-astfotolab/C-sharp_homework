2060c96067f0c21b1d4e0b47cdb277904cc809ec
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2022-12-23 13:06:02 +0400

    add task3_HW3

---
+++ HomeWork03/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿//ДЗ задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Clear();
+
+for(int i = 1; i <= x; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}, ");
+}
