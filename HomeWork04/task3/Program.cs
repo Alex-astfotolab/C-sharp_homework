2a7dbe160bff837f2678e492e9ad8ef269fcc5cc
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2023-01-13 15:46:23 +0400

    add HomeWork04 task3

---
+++ HomeWork04/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] GetArrey(int n)
+{
+    int[] NumArrey = new int[n];
+    for(int i =0; i < n; i++)
+    {
+        NumArrey[i] = new Random().Next(-1000,1000);
+    }
+    return NumArrey;
+}
+
+int[] x = GetArrey(8);
+Console.WriteLine($"[{String.Join("|", x)}]");
