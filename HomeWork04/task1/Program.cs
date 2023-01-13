ceda07ae3b1e1ac3e90b4feb30d6114a07106d76
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2023-01-13 13:26:13 +0400

    add HomeWork04 task1

---
+++ HomeWork04/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetDegree(int a, int b)
+{
+    int res = 1;
+    int i = 0;
+    while( i < b)
+    {
+        res = res * a;
+        i++;
+    }
+    return res;
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Clear();
+Console.Write("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.Clear();
+Console.WriteLine($"число {x} в степени {y}: {GetDegree(x, y)}");
