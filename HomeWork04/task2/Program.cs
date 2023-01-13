4608b36569d187d14a0e3c393be78e13f9dff3aa
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2023-01-13 15:40:52 +0400

    add HomeWork04 task2

---
+++ HomeWork04/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetSum(int x)
+    {
+        int sum = 0;
+        while(x > 0)
+        {
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    }
+
+Console.Clear();
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Сумма знаков числа {x}: {GetSum(x)}");
