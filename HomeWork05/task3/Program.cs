5e143c33c3c6dd3c1a5cc2bfd0e56a2ca8f80c54
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2023-01-17 15:40:07 +0400

    add HomeWork05 task3

---
+++ HomeWork05/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+int GetSumEl(int[] array)
+{
+    int x = array[0] * array[3];
+    int y = array[1] * array[2];
+    
+    
+
+}
+
+int[] array = GetArray(4, 1, 10);
+Console.WriteLine($"[{String.Join(",", array)}]");
+Console.WriteLine($"{GetSumEl(array)}");
