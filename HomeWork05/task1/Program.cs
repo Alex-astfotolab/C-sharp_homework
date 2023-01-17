d9b3a7fdbfcf11d0fe2f9bb8ecbd07a865615986
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2023-01-17 13:55:30 +0400

    add HomeWork05 task2

--- HomeWork05/task1/Program.cs
+++ HomeWork05/task1/Program.cs
@@ -11,14 +11,14 @@
 int resault(int[] array)
 {
     int count = 0;
-    foreach(int el in array)
+    foreach (int el in array)
     {
-        if(el > 0 && el > 100 && el % 2 == 0)
+        if (el > 0 && el > 100 && el % 2 == 0)
         {
             count++;
         }
-        
-    } 
+
+    }
     return count;
 }
 Console.Clear();
---
+++ HomeWork05/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetSum(int[] array)
+{
+    int resault = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            resault = resault + array[i];
+        }
+    }
+    return resault;
+}
+
+int[] array = GetArray(4, -500, 500);
+Console.Clear();
+Console.WriteLine($"[{String.Join(".", array)}]");
+Console.WriteLine($"Сумма элементов нечетных индексов массива: {GetSum(array)}");
