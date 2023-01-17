6bdfdcdd626891a9c096e569eff2901877ffa4a0
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2023-01-17 15:49:44 +0400

    add HomeWork05 task2

--- HomeWork05/task3/Program.cs
+++ HomeWork05/task3/Program.cs
@@ -7,13 +7,12 @@
     }
     return res;
 }
-int GetSumEl(int[] array)
+int[] GetSumEl(int[] array)
 {
     int x = array[0] * array[3];
     int y = array[1] * array[2];
-    
-    
-
+    int[] a = new int[] {x, y};
+return a;
 }
 
 int[] array = GetArray(4, 1, 10);
