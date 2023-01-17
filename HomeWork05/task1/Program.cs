e34a54bfc566086b889985bd4101c2c34cfc0289
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2023-01-17 12:19:44 +0400

    add HomeWork05 task1

---
+++ HomeWork05/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int resault(int[] array)
+{
+    int count = 0;
+    foreach(int el in array)
+    {
+        if(el > 0 && el > 100 && el % 2 == 0)
+        {
+            count++;
+        }
+        
+    } 
+    return count;
+}
+Console.Clear();
+int[] array = GetArray(6, 1, 1000);
+Console.WriteLine($"[{String.Join(",", array)}]");
+Console.WriteLine($"из них четных: {resault(array)}");
