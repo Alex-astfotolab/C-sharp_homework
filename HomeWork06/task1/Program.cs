7e3b2a2ff646f1d9d9af5db76fa51b177ba0c612
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2023-01-23 11:45:04 +0400

    add HomeWork06_task01

---
+++ HomeWork06/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите любые числа, через пробел: ");
+Console.Clear();
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+
+Console.WriteLine($"чисел больше 0: {count}");
